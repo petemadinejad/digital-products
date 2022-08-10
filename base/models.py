1ecd0c3832fdc6f85015cc184fa8423e6cbfac2c
parisaetemadinejad <parisa.etemadinejad@gmail.com>
2022-08-10 14:51:37 +0430

    model completed

--- base/models.py
+++ base/models.py
@@ -1,9 +1,10 @@
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
 
 
 class BaseModel(models.Model):
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(verbose_name=_('Created at'), auto_now_add=True)
+    updated_at = models.DateTimeField(verbose_name=_('Updated at'), auto_now=True)
 
     class Meta:
         abstract = True
--- products/models.py
+++ products/models.py
@@ -1,30 +1,46 @@
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
-
+from base.models import BaseModel
 
 class Product(BaseModel):
-    name = models.CharField(max_length=255)
-    description = models.TextField(blank=True)
-    price = models.DecimalField(max_digits=6, decimal_places=2)
-    image = models.ImageField(upload_to="products/", blank=True)
+    title = models.CharField(verbose_name=_('Title'), max_length=50)
+    description = models.TextField(verbose_name=_('Description'), blank=True)
+    avatar = models.ImageField(verbose_name=_('Avatar'), upload_to='product/', blank=True)
+    is_enable = models.BooleanField(verbose_name=_('Is enable'), default=True)
+    categories = models.ManyToManyField('Category', verbose_name=_('Categories'), blank=True)
 
     def __str__(self):
         return self.name
 
 
 class Category(BaseModel):
+    parent = models.ForeignKey('self', on_delete=models.CASCADE, blank=True, null=True)
     title = models.CharField(verbose_name=_('Title'), max_length=50)
     description = models.TextField(verbose_name=_('Description'), blank=True)
-    avatar=models.imageField(verbose_name=_('Avatar'), upload_to='category/', blank=True)
+    avatar = models.ImageField(verbose_name=_('Avatar'), upload_to='category/', blank=True)
     is_enable = models.BooleanField(verbose_name=_('Is enable'), default=True)
 
     def __str__(self):
         return self.title
 
+    class Meta:
+        db_table = 'category'
+        verbose_name = _('Category')
+        verbose_name_plural = _('Categories')
+        ordering = ['title']
+
 
 class File(BaseModel):
-    name = models.CharField(max_length=255)
-    file = models.FileField(upload_to="files/")
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, verbose_name=_('Product'))
+    title = models.CharField(verbose_name=_('Title'), max_length=50)
+    file = models.FileField(verbose_name=_('File'), upload_to='file/%Y/%m/%d/')
+    is_enable = models.BooleanField(verbose_name=_('Is enable'), default=True)
 
     def __str__(self):
         return self.name
+
+    class Meta:
+        db_table = 'file'
+        verbose_name = _('File')
+        verbose_name_plural = _('Files')
+        ordering = ['title']

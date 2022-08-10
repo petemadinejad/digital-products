9e80bba05e8a7f0e89d3d8bc030f75562d899d85
parisaetemadinejad <parisa.etemadinejad@gmail.com>
2022-08-10 14:30:43 +0430

    add base model

--- products/models.py
+++ products/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
 
-# Create your models here.
+
+class Product(BaseModel):
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    image = models.ImageField(upload_to="products/", blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Category(BaseModel):
+    title = models.CharField(verbose_name=_('Title'), max_length=50)
+    description = models.TextField(verbose_name=_('Description'), blank=True)
+    avatar=models.imageField(verbose_name=_('Avatar'), upload_to='category/', blank=True)
+    is_enable = models.BooleanField(verbose_name=_('Is enable'), default=True)
+
+    def __str__(self):
+        return self.title
+
+
+class File(BaseModel):
+    name = models.CharField(max_length=255)
+    file = models.FileField(upload_to="files/")
+
+    def __str__(self):
+        return self.name

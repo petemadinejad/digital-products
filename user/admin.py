469c9035b4b968fc7ba832dca8ac95c18b6654f9
petemadinejad <parisaetemadinejad.job@gmail.com>
2022-08-18 20:04:48 +0430

    add group model from django.contrib.auth.models

--- user/admin.py
+++ user/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.utils.translation import gettext_lazy as _
+from django.contrib.auth.models import Group
 
 from .models import *
 
